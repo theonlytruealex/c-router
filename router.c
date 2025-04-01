ee1b63a8725253633614f74d0eac72d27049ebd2
theonlytruealex <alexiova264@gmail.com>
2025-04-01 11:08:09 +0300

    Finished IPV4 Trie, no icmp

--- router.c
+++ router.c
@@ -2,48 +2,53 @@
 #include "queue.h"
 #include "lib.h"
 #include <arpa/inet.h>
-#include <stdbool.h>
+#include <string.h>
 
 #define IPV4_TYPE 0x0800
 #define ARP_TYPE 0x0806
-#define ETH_HDR_SIZE 0x20
+#define ETH_HDR_SIZE 0xe
 
 typedef struct trie_node {
-	bool has_children;
+	short has_children;
 	struct trie_node *children[2];
 	struct route_table_entry *entry;
 } trie_node;
 
 void init_node(trie_node* root) {
-	root->has_children = false;
+	root->has_children = 0;
 	root->children[0] = NULL;
 	root->children[1] = NULL;
 	root->entry = NULL;
 }
+void print_binary(unsigned int num) {
+    for (int i = 31; i >= 0; i--) {
+        printf("%d", (num >> i) & 1);
+    }
+    printf("\n");
+}
 
-void insert(trie_node* root, struct route_table_entry *entry, int position) {
-	if (entry->mask << position == 0) {
+void insert(trie_node* root, struct route_table_entry *entry, uint32_t mask, uint32_t prefix, int position) {
+	if ((mask & (1 << (31 - position))) == 0 || position == 32) {
 		root->entry = entry;
 		return;
 	}
-
-	u_int32_t prefifx = entry->prefix;
-	bool byte = ((prefifx << position) & 2147483648) >> 31;
+	short byte = ((prefix << position) & 2147483648) >> 31;
 
 	if (!root->has_children)
-		root->has_children = true;
+		root->has_children = 1;
 
-	if (root->children[byte] == NULL)
+	if (root->children[byte] == NULL){
 		root->children[byte] = (trie_node *)malloc(sizeof(trie_node));
-
-	insert(root->children[byte], entry, position + 1);
+		init_node(root->children[byte]);
+	}
+	insert(root->children[byte], entry, mask, prefix, position + 1);
 }
 
 struct route_table_entry* get_table_entry(trie_node* root, u_int32_t address, int position) {
 	if (!root->has_children)
 		return root->entry;
 	
-	bool byte = ((address << position) & 2147483648) >> 31;
+	short byte = ((address << position) & 2147483648) >> 31;
 
 	if (root->children[byte] == NULL)
 		return root->entry;
@@ -91,20 +96,18 @@ int main(int argc, char *argv[])
 	init(argv + 2, argc - 2);
 
 	struct route_table_entry *rtable = (struct route_table_entry *)malloc(sizeof(struct route_table_entry) * 80001);
-	struct arp_table_entry *arp_table = (struct arp_table_entry *)malloc(sizeof(struct arp_table_entry) * 3000);
+	struct arp_table_entry *arp_table = (struct arp_table_entry *)malloc(sizeof(struct arp_table_entry) * 30000);
 
-	parse_arp_table("/home/alex/Documents/pcom/homework1-public/arp_table.txt", arp_table);
+	int arp_len = parse_arp_table("./arp_table.txt", arp_table);
 
 	int table_len = read_rtable(argv[1], rtable);
 
-	trie_node *root;
+	trie_node *root = (trie_node *)malloc(sizeof(trie_node));
 	init_node(root);
 
 	for (int i = 0; i < table_len; i++) {
-		insert(root, &rtable[i], 0);
+		insert(root, &rtable[i], ntohl(rtable[i].mask), ntohl(rtable[i].prefix), 0);
 	}
-
-
 	while (1) {
 
 		size_t interface;
@@ -112,7 +115,6 @@ int main(int argc, char *argv[])
 
 		interface = recv_from_any_link(buf, &len);
 		DIE(interface < 0, "recv_from_any_links");
-
 		struct ether_hdr *eth_header = (struct ether_hdr *) buf;
 		if (drop_dest_mac(eth_header->ethr_dhost, interface)) {
 			drop_packet(buf);
@@ -125,34 +127,46 @@ int main(int argc, char *argv[])
 		any header field which has more than 1 byte will need to be conerted to
 		host order. For example, ntohs(eth_hdr->ether_type). The oposite is needed when
 		sending a packet on the link, */
-
 		if (ntohs(eth_header->ethr_type) == IPV4_TYPE) {
 			struct ip_hdr *ip_header = (struct ip_hdr *)(buf + ETH_HDR_SIZE);
 			
-			uint16_t old_sum = ip_header->checksum;
+			uint16_t old_sum = ntohs(ip_header->checksum);
 
 			ip_header->checksum = 0;
 			if (old_sum != checksum((uint16_t *)ip_header, sizeof(struct ip_hdr)))
 				continue;
-
 			if (ip_header->dest_addr == inet_addr(get_interface_ip(interface))) {
 				icmp_response();
 				continue;
 			}
-
 			if (ip_header->ttl == 0 || ip_header->ttl == 1) {
 				icmp_response();
 				continue;
 			}
 			ip_header->ttl -= 1;
 
-			struct route_table_entry *entry = get_table_entry(root, ip_header->dest_addr, 0);
+			struct route_table_entry *entry = get_table_entry(root, ntohl(ip_header->dest_addr), 0);
 			if (entry == NULL) {
 				icmp_response();
 				continue;
 			}
+			struct arp_table_entry *arp_entry;
+
+			for (int i = 0; i < arp_len; i++) {
+				if (entry->next_hop == arp_table[i].ip){
+					arp_entry = &arp_table[i];
+					break;
+				}
+			}
+			uint8_t mac[6];
+			get_interface_mac(interface, mac);
+
+			memcpy(eth_header->ethr_dhost, arp_entry->mac, 6);
+			memcpy(eth_header->ethr_shost, mac, 6);
+
 			ip_header->checksum = 0;
-			ip_header->checksum = checksum((uint16_t *)ip_header, sizeof(struct ip_hdr));
+			ip_header->checksum = htons(checksum((uint16_t *)ip_header, sizeof(struct ip_hdr)));
+			send_to_link(len, buf, entry->interface);
 		}
 
 	}
